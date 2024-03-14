5502684548851c06b190b4294fa19189cf029f5e
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2024-03-14 18:58:17 -0300

    Tarefa 5 - Criando a classe de Produto e Fabricante

---
+++ src/main/java/br/com/alurasenac/farmacia_spring/orm/produto.java
@@ -0,0 +1,49 @@
+package br.com.alurasenac.farmacia_spring.orm;
+
+public class produto {
+    private int id;
+    private String name;
+    private String descricao;
+    private double preco;
+    private Fabricante fabricante;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+}
